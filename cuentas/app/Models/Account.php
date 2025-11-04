331cf46a7e72455edda4a79fa6a35a09e830bc13
AaronSotoPacheco <111542116+AaronSotoPacheco@users.noreply.github.com>
2025-11-04 15:44:53 -0700

    jalesillo

---
+++ cuentas/app/Models/Account.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+// Un Account pertenece a un User y tiene muchas Transactions.
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user(){
+        return $this->hasOne (User::class,'id','user_id');
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+}
---
+++ cuentas/app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+// Una Category pertenece a un User y tiene muchas Transactions.
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user(){
+    
+        return $this-> hasOne (User::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
